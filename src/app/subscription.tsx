e59d07b15cf65c4a71940eedc641c6aa1c3fca4b
cedric odera <cedricodera@gmail.com>
2026-08-01 10:14:24 +0300

    Changes to the pricing page

--- src/app/subscription.tsx
+++ src/app/subscription.tsx
@@ -360,7 +360,7 @@ function ComparisonTable({
                   >
                     {config.price.toLocaleString()}
                   </Text>
-                  <Text style={tableStyles.priceSub}>per month</Text>
+                  <Text style={tableStyles.priceSub}>Per Month</Text>
                 </>
               )}
             </View>
